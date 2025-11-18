5da1b113f6021b2e0771a55f5ba0a9713c6843ad
GhaithSomrani <ghaithd02@gmail.com>
2025-11-18 11:43:35 +0100

    fix theme card

--- lib/config/app_theme.dart
+++ lib/config/app_theme.dart
@@ -23,7 +23,7 @@ class AppTheme {
         elevation: 0,
         centerTitle: true,
       ),
-      cardTheme: const CardTheme(
+      cardTheme: CardThemeData(
         elevation: 2,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(12),
